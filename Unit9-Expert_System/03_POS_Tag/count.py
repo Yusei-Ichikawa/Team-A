d1141da07c5b87e59ce81ca24a5559e9cb49bdde
SeeQuadSir <hp.personalcomputer.mine@gmail.com>
2023-08-07 11:28:40 +0900

    fix count

--- Unit9-Expert_System/03_POS_Tag/count.py
+++ Unit9-Expert_System/03_POS_Tag/count.py
@@ -3,7 +3,7 @@
 # identify the least similiar dataset based on the number of identical POS patterns with questionned dataset
 # suggest ruling out the least similiar dataset
 
-import nltk
+from Tag_POS import get_pos_tags
 
 # nltkの全タグ
 tag_list = tuple(
@@ -55,17 +55,6 @@ tag_list = tuple(
         "``",
     ]
 )
-# following will be overwritten when tagging is completed
-sentence = """At eight o'clock on Thursday morning 
-            Arthur didn't feel very good."""
-tokens = nltk.word_tokenize(sentence)
-sentence = """identify the least similiar dataset based on 
-            the number of identical POS patterns with questionned dataset"""
-tokens_2 = nltk.word_tokenize(sentence)
-
-tagged_1 = nltk.pos_tag(tokens)
-tagged_2 = nltk.pos_tag(tokens_2)
-tagged_q = nltk.pos_tag(tokens)
 
 
 # POSタグとその数を名前付きで保存
@@ -86,7 +75,8 @@ def count_pos(tagged: list):
     return pn_dict
 
 
-# 2つのデータの(タグ数/総トークン数)を比較
+# 2つのデータの(各タグ数/総トークン数)を比較
+# ズレの総和を返す
 def compare_pos(pn1: dict, tn1: int, pn2: dict, tn2: int):
     dev = 0
     for tag in tag_list:
@@ -106,14 +96,17 @@ def compare_pos(pn1: dict, tn1: int, pn2: dict, tn2: int):
 
 # kwargs == ds1:Dataset, ds2:Dataset,...
 # 対象と既存データセットを比較、類似度が最も低いデータ名とズレの値を返す
-def find_unlikely(pnq: dict, tnq: int, **kwargs):
+def find_unlikely(dsq: Dataset, **kwargs: Dataset):
     max_dev = 0
     unl_dict = None
+    tnq = 0
+    for v in dsq.pn_dict.values():
+        tnq += v
     for ds in kwargs.values():
         tn = 0
         for v in ds.pn_dict.values():
             tn += v
-        dev = compare_pos(pnq, tnq, ds.pn_dict, tn)
+        dev = compare_pos(dsq.pn_dict, tnq, ds.pn_dict, tn)
         if dev > max_dev:
             max_dev = dev
             unl_dict = ds.name
@@ -121,13 +114,17 @@ def find_unlikely(pnq: dict, tnq: int, **kwargs):
 
 
 # 以上を実行する
-# 対象と既知データのファイル名を受け取って実行するように修正予定
 def count():
-    pn_dict_1 = count_pos(tagged_1)
-    pn_dict_2 = count_pos(tagged_2)
-    pn_dict_q = count_pos(tagged_q)
+    taggedq = get_pos_tags("Q dataset.txt")
+    tagged1 = get_pos_tags("K1 dataset.txt")
+    tagged2 = get_pos_tags("K2 dataset.txt")
 
+    pn_dict_q = count_pos(taggedq)
+    pn_dict_1 = count_pos(tagged1)
+    pn_dict_2 = count_pos(tagged2)
+
+    dsq = Dataset("Q", pn_dict_q)
     ds1 = Dataset("K1", pn_dict_1)
     ds2 = Dataset("K2", pn_dict_2)
 
-    return find_unlikely(pn_dict_q, len(tokens), ds1=ds1, ds2=ds2)
+    return find_unlikely(dsq=dsq, ds1=ds1, ds2=ds2)
